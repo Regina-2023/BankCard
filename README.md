e7bd99c0dbfae9dcea48f76176e1aa710a665467
Regina Faritova <faritova_regina@mail.ru>
2023-07-29 14:39:16 +0300

    Add badge.

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/rn55m3as9y892d9p?svg=true)](https://ci.appveyor.com/project/Regina-2023/bankcard)
---
+++ src/test/java/BankCardTest.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class BankCardTest {
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[id=root]");
+        form.$("input[name='name']").setValue("Регина Фаритова");
+        form.$("input[name='phone']").setValue("+79117790793");
+        form.$("span[class='checkbox__box']").click();
+        form.$("button[type='button']").click();
+        $("p[data-test-id='order-success']").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+}
